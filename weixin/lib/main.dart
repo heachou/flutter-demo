0337c7601d908bda75d1a7ca54482527a11e09a2
axl <1544500164@qq.com>
2019-01-28 16:30:11 +0800

    完成基本的布局

---
+++ weixin/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import './home/home_screen.dart';
+import './home/constants.dart' show AppColors;
+
+void main() => runApp(MaterialApp(
+  title: '微信',
+  theme: ThemeData.light().copyWith(
+    primaryColor: Color(AppColors.AppbarColor)
+  ),
+  home: HomeScreen(),
+));
